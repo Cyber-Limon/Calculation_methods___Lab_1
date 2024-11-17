7be5ee41e7b46fc36814cafc3400b6d946f314f7
Max <162749285+Cyber-Limon@users.noreply.github.com>
2024-11-17 18:01:14 +0500

    Update Calculation_methods_1.cpp

--- Calculation_methods_1.cpp
+++ Calculation_methods_1.cpp
@@ -1,4 +1,4 @@
-﻿#define _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
 
 #include <iostream>
 #include <cmath>
@@ -7,36 +7,31 @@
 
 using namespace std;
 
-class Point {
-private:
-	double x, y;
+double S(double array_x[], double array_y[], double array_m[], double x, double h) {
+	int i = 0;
+	while (x > array_x[i])
+		i++;
 
-public:
-	Point() {
-		x = 0;
-		y = 0;
-	}
+	double x1 = array_x[i - 1];
+	double y1 = array_y[i - 1];
+	double x2 = array_x[i];
+	double y2 = array_y[i];
 
-	void add_value(double n) {
-		x = n;
-		y = sin(n);
-	}
-
-	double get_x() {
-		return x;
-	}
-
-	double get_y() {
-		return y;
-	}
-};
-
-
-
-double S(double x, double x1, double y1, double x2, double y2, double array_m[], int i, double h) {
-	return ((x2 - x) * (x2 - x) * (x2 - x) - h * h * (x2 - x)) * array_m[i - 1] / (6 * h) +
+	double S = ((x2 - x) * (x2 - x) * (x2 - x) - h * h * (x2 - x)) * array_m[i - 1] / (6 * h) +
 		   ((x - x1) * (x - x1) * (x - x1) - h * h * (x - x1)) * array_m[i] / (6 * h) +
 		   (x2 - x) * y1 / h + (x - x1) * y2 / h;
+
+	return S;
+}
+
+
+
+int exponent(double exp) {
+	if (exp < 1.5) return 1;
+	else if (exp < 3) return 2;
+	else if (exp < 6) return 4;
+	else if (exp < 12) return 8;
+	else return 16;
 }
 
 
@@ -45,43 +40,45 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 
 	cout << "n\t" << "d_max\t\t" << "d_оц\t\t" << "K_d\n";
-
-	for (int i = 0; i < 50; i++)
-		cout << "-";
-
+	for (int i = 0; i < 50; i++) cout << "-";
 	cout << "\n";
+
 
 
 	double* array_d_max = new double[0];
 
 	double prev_d_max = 0;
+	double d_max_n = 0;
 	double d_ev = 0;
 	double K_d = 0;
 
-	int k;
+
 
 	for (int n = 5; n < 11000; n *= 2) {
-
 		int count_segments = n;
-
-		Point* array_points = new Point[count_segments + 1];
-
-		double *array_a = new double[count_segments + 1];
-		double *array_b = new double[count_segments + 1];
-		double *array_c = new double[count_segments + 1];
-		double *array_d = new double[count_segments + 1];
-
-		double *array_l = new double[count_segments + 1];
-		double *array_w = new double[count_segments + 1];
-
-		double *array_m = new double[count_segments + 1];
-
 		double h = M_PI / count_segments;
 
 
 
-		for (int i = 0; i <= count_segments; i++)
-			array_points[i].add_value(h * i);
+		double* array_x = new double[count_segments + 1];
+		double* array_y = new double[count_segments + 1];
+
+		double* array_a = new double[count_segments + 1];
+		double* array_b = new double[count_segments + 1];
+		double* array_c = new double[count_segments + 1];
+		double* array_d = new double[count_segments + 1];
+
+		double* array_l = new double[count_segments + 1];
+		double* array_w = new double[count_segments + 1];
+
+		double* array_m = new double[count_segments + 1];
+
+		
+
+		for (int i = 0; i <= count_segments; i++) {
+			array_x[i] = h * i;
+			array_y[i] = sin(h * i);
+		}
 
 
 
@@ -94,7 +91,7 @@ int main() {
 			array_a[i] = 2 * h / 3;
 			array_b[i] = h / 6;
 			array_c[i] = h / 6;
-			array_d[i] = (array_points[i + 1].get_y() - 2 * array_points[i].get_y() + array_points[i - 1].get_y()) / h;
+			array_d[i] = (array_y[i + 1] - 2 * array_y[i] + array_y[i - 1]) / h;
 		}
 
 
@@ -117,33 +114,22 @@ int main() {
 
 
 		if (prev_d_max != 0) {
-			double d_max_n = 0;
+			d_max_n = 0;
 
 			for (int n = 1; n < count_segments; n += 2) {
-				double x = h * n;
-
 				int m = (n - 1) / 2;
 
-				int i = 0;
-				while (x > array_points[i].get_x())
-					i++;
+				double x = h * n;
 
-				double x1 = array_points[i - 1].get_x();
-				double y1 = array_points[i - 1].get_y();
-				double x2 = array_points[i].get_x();
-				double y2 = array_points[i].get_y();
+				double s = S(array_x, array_y, array_m, x, h);
 
-				double s = S(x, x1, y1, x2, y2, array_m, i, h);
-
-				if ((s - array_d_max[m]) > d_max_n)
-					d_max_n = s - array_d_max[m];
+				if (abs(s - array_d_max[m]) > d_max_n)
+					d_max_n = abs(s - array_d_max[m]);
 			}
-
-			d_ev = d_max_n / 16;
 		}
 
 		delete[] array_d_max;
-		double *new_array_d_max = new double[count_segments];
+		double* new_array_d_max = new double[count_segments];
 		array_d_max = new_array_d_max;
 
 
@@ -153,16 +139,7 @@ int main() {
 		for (int n = 0; n < count_segments; n++) {
 			double x = h * n + h / 2;
 
-			int i = 0;
-			while (x > array_points[i].get_x())
-				i++;
-
-			double x1 = array_points[i - 1].get_x();
-			double y1 = array_points[i - 1].get_y();
-			double x2 = array_points[i].get_x();
-			double y2 = array_points[i].get_y();
-
-			double s = S(x, x1, y1, x2, y2, array_m, i, h);
+			double s = S(array_x, array_y, array_m, x, h);
 
 			array_d_max[n] = s;
 
@@ -173,18 +150,24 @@ int main() {
 
 
 		if (prev_d_max != 0) {
-			K_d = prev_d_max / d_max; 
+			K_d = prev_d_max / d_max;
+
+			d_ev = d_max_n / exponent(K_d);
 		}
 
 		prev_d_max = d_max;
 
 
 
-		cout << n << "\t" << d_max << "\t" << d_ev << "\t" << K_d << "\n";
+		if (d_ev != 0)
+			cout << n << "\t" << d_max << "\t" << d_ev << "\t" << K_d << "\n";
+		else
+			cout << n << "\t" << d_max << "\t" << "-\t\t" << "-\n";
 
 
 
-		delete[] array_points;
+		delete[] array_x;
+		delete[] array_y;
 
 		delete[] array_a;
 		delete[] array_b;
